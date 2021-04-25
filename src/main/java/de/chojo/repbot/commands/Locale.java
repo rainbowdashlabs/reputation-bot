34d228dd2f0fa32bd0980d15357e1d8ccfac34a4
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-04-25 14:47:36 +0200

    Add locale command

--- src/main/java/de/chojo/repbot/commands/Locale.java
+++ src/main/java/de/chojo/repbot/commands/Locale.java
@@ -1,2 +1,71 @@
-package de.chojo.repbot.commands;public class Locale {
+package de.chojo.repbot.commands;
+
+import de.chojo.jdautil.command.SimpleCommand;
+import de.chojo.jdautil.localization.Localizer;
+import de.chojo.jdautil.localization.util.Format;
+import de.chojo.jdautil.localization.util.Language;
+import de.chojo.jdautil.localization.util.Replacement;
+import de.chojo.jdautil.text.TextFormatting;
+import de.chojo.jdautil.wrapper.CommandContext;
+import de.chojo.jdautil.wrapper.MessageEventWrapper;
+import de.chojo.repbot.data.GuildData;
+import net.dv8tion.jda.api.Permission;
+
+import javax.sql.DataSource;
+
+public class Locale extends SimpleCommand {
+    private final GuildData data;
+    private final Localizer loc;
+
+    public Locale(DataSource dataSource, Localizer loc) {
+        super("locale",
+                null,
+                "command.locale.description",
+                "", subCommandBuilder()
+                        .add("set", "<locale>", "command.locale.sub.set")
+                        .add("list", null, "command.locale.sub.list")
+                        .build(),
+                Permission.ADMINISTRATOR);
+        this.data = new GuildData(dataSource);
+        this.loc = loc;
+    }
+
+    @Override
+    public boolean onCommand(MessageEventWrapper eventWrapper, CommandContext context) {
+        if (context.argsEmpty()) {
+            var guildLocale = loc.getGuildLocale(eventWrapper.getGuild());
+            eventWrapper.reply(eventWrapper.localize("command.locale.current",
+                    Replacement.create("LOCALE", guildLocale.getLanguage()))).queue();
+            return true;
+        }
+
+        var subCmd = context.argString(0).get();
+        if (subCmd.equalsIgnoreCase("set")) {
+            return set(eventWrapper, context.subContext(subCmd));
+        }
+
+        return false;
+    }
+
+    private boolean set(MessageEventWrapper eventWrapper, CommandContext context) {
+        if (context.argsEmpty()) return false;
+        var language = loc.getLanguage(context.argString(0).get());
+        if (language.isEmpty()) {
+            eventWrapper.replyErrorAndDelete(eventWrapper.localize("command.locale.error.invalidLocale"), 10);
+            return true;
+        }
+        data.setLanguage(eventWrapper.getGuild(), language.get());
+        eventWrapper.reply(eventWrapper.localize("command.locale.sub.set.set",
+                Replacement.create("LOCALE", language.get().getLanguage(), Format.CODE))).queue();
+        return false;
+    }
+
+    private boolean list(MessageEventWrapper eventWrapper, CommandContext context) {
+        var languages = loc.getLanguages();
+        var builder = TextFormatting.getTableBuilder(languages,
+                eventWrapper.localize("words.language"), eventWrapper.localize("words.code"));
+        languages.forEach(l -> builder.setNextRow(l.getLanguage(), l.getCode()));
+        eventWrapper.reply(eventWrapper.localize("command.locale.sub.list.list") + "\n" + builder).queue();
+        return false;
+    }
 }
