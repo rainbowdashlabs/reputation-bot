40e4f22162fc61abdc7d3be11252b6dea90fb494
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2021-05-26 14:39:59 +0200

    Make top list mobile friendly #40 (#42)
    * Make top list mobile friendly #40

--- src/main/java/de/chojo/repbot/data/wrapper/ReputationUser.java
+++ src/main/java/de/chojo/repbot/data/wrapper/ReputationUser.java
@@ -2,6 +2,7 @@ package de.chojo.repbot.data.wrapper;
 
 import lombok.Getter;
 import net.dv8tion.jda.api.entities.User;
+import org.apache.commons.lang3.StringUtils;
 
 @Getter
 public class ReputationUser {
@@ -17,5 +18,15 @@ public class ReputationUser {
 
     public static ReputationUser empty(User user) {
         return new ReputationUser(0, user.getIdLong(), 0);
+    }
+
+    public User userFromId() {
+        return User.fromId(userId);
+    }
+
+    public String fancyString(int maxRank) {
+        var length = String.valueOf(maxRank).length();
+        var rank = StringUtils.rightPad(String.valueOf(this.rank), length);
+        return "`" + rank + "` **|** " + userFromId().getAsMention() + " ➜ " + reputation;
     }
 }
