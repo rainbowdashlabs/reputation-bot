4133c2322a17080e8d50c40a5acf8d81c1e82edd
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-02 17:39:22 +0200

    Port Locale command

--- src/main/java/de/chojo/repbot/commands/Locale.java
+++ src/main/java/de/chojo/repbot/commands/Locale.java
@@ -56,7 +56,13 @@ public class Locale extends SimpleCommand {
 
     @Override
     public void onSlashCommand(SlashCommandEvent event) {
-
+        var subCmd = event.getSubcommandName();
+        if ("set".equalsIgnoreCase(subCmd)) {
+            set(event);
+        }
+        if ("list".equalsIgnoreCase(subCmd)) {
+            list(event);
+        }
     }
 
     private boolean set(MessageEventWrapper eventWrapper, CommandContext context) {
@@ -78,6 +84,30 @@ public class Locale extends SimpleCommand {
                 eventWrapper.localize("words.language"), eventWrapper.localize("words.code"));
         languages.forEach(l -> builder.setNextRow(l.getLanguage(), l.getCode()));
         eventWrapper.reply(eventWrapper.localize("command.locale.sub.list.list") + "\n" + builder).queue();
+        return true;
+
+    }
+
+    private boolean set(SlashCommandEvent event) {
+        var loc = this.loc.getContextLocalizer(event.getGuild());
+        var language = this.loc.getLanguage(event.getOption("language").getAsString());
+        if (language.isEmpty()) {
+            event.reply(loc.localize("command.locale.error.invalidLocale")).setEphemeral(true).queue();
+            return true;
+        }
+        data.setLanguage(event.getGuild(), language.get());
+        event.reply(loc.localize("command.locale.sub.set.set",
+                Replacement.create("LOCALE", language.get().getLanguage(), Format.CODE))).queue();
+        return true;
+    }
+
+    private boolean list(SlashCommandEvent event) {
+        var loc = this.loc.getContextLocalizer(event.getGuild());
+        var languages = this.loc.getLanguages();
+        var builder = TextFormatting.getTableBuilder(languages,
+                loc.localize("words.language"), loc.localize("words.code"));
+        languages.forEach(l -> builder.setNextRow(l.getLanguage(), l.getCode()));
+        event.reply(loc.localize("command.locale.sub.list.list") + "\n" + builder).queue();
         return true;
     }
 }
