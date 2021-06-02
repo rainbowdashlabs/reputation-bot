6c6b7de21b37c747f41bd94ce600948d5b6aaef7
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-02 17:10:08 +0200

    Fix help command

--- src/main/java/de/chojo/repbot/commands/Help.java
+++ src/main/java/de/chojo/repbot/commands/Help.java
@@ -1,7 +1,10 @@
 package de.chojo.repbot.commands;
 
+import de.chojo.jdautil.command.SimpleArgument;
 import de.chojo.jdautil.command.SimpleCommand;
 import de.chojo.jdautil.listener.CommandHub;
+import de.chojo.jdautil.localization.ContextLocalizer;
+import de.chojo.jdautil.localization.ILocalizer;
 import de.chojo.jdautil.localization.Localizer;
 import de.chojo.jdautil.localization.util.Format;
 import de.chojo.jdautil.localization.util.LocalizedEmbedBuilder;
@@ -9,6 +12,7 @@ import de.chojo.jdautil.localization.util.Replacement;
 import de.chojo.jdautil.wrapper.CommandContext;
 import de.chojo.jdautil.wrapper.MessageEventWrapper;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
@@ -23,6 +27,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Help extends SimpleCommand {
+    private static final String REQ = "<%s>";
+    private static final String OPT = "[%s]";
     private final CommandHub<SimpleCommand> hub;
     private final Localizer loc;
 
@@ -67,16 +73,17 @@ public class Help extends SimpleCommand {
         var eventWrapper = MessageEventWrapper.create(event);
 
         if (event.getOptions().isEmpty()) {
-            MessageEmbed message = getAllCommandsEmbed(eventWrapper);
+            var message = getAllCommandsEmbed(eventWrapper);
             event.reply(wrap(message)).queue();
-            eventWrapper.reply(message).queue();
             return;
         }
 
         @SuppressWarnings("ConstantConditions")
         var cmd = event.getOption("command").getAsString();
         var command = hub.getCommand(cmd);
-        if (command.isEmpty() || !eventWrapper.getMember().hasPermission(command.get().permission())) {
+        if (command.isEmpty() ||
+                (!eventWrapper.getMember().hasPermission(command.get().permission())
+                        && command.get().permission() != Permission.UNKNOWN)) {
             event.reply(eventWrapper.localize("error.commandNotFound"))
                     .delay(Duration.ofSeconds(10))
                     .flatMap(InteractionHook::deleteOriginal)
@@ -107,15 +114,7 @@ public class Help extends SimpleCommand {
                         Replacement.create("COMMAND", command.command())))
                 .setDescription(command.description());
 
-        if (command.alias().length > 0) {
-            var aliases = Arrays.stream(command.alias())
-                    .map(s -> StringUtils.wrap(s, "`"))
-                    .collect(Collectors.joining(", "));
-            embedBuilder.addField("command.help.alias", aliases, false);
-        }
 
-
-        // TODO: 28.05.2021 Fix it
         if (command.alias() != null) {
             embedBuilder.addField("command.help.usage", command.alias() + " " + command.args(), false);
         }
@@ -130,5 +129,43 @@ public class Help extends SimpleCommand {
             embedBuilder.addField("command.help.subCommands", String.join("\n", subCommands), false);
         }
         return embedBuilder.build();
+    }
+
+    public MessageEmbed getCommandHelp(SimpleCommand command, ILocalizer localizer, Guild guild) {
+        var loc = localizer.getContextLocalizer(guild);
+
+        var builder = new LocalizedEmbedBuilder(localizer, guild)
+                .setTitle(loc.localize("command.help.title",
+                        Replacement.create("COMMAND", command.command())))
+                .setDescription(loc.localize(description()));
+
+        if (command.alias().length > 0) {
+            var aliases = Arrays.stream(command.alias())
+                    .map(s -> StringUtils.wrap(s, "`"))
+                    .collect(Collectors.joining(", "));
+            builder.addField("command.help.alias", aliases, false);
+        }
+
+        if (subCommands() != null) {
+            List<String> commands = new ArrayList<>();
+            for (var simpleSubCommand : subCommands()) {
+                commands.add(simpleSubCommand.name() + " " + argsAsString(simpleSubCommand.args(), loc));
+            }
+            builder.addField("command.help.subCommands", String.join("\n", commands), false);
+        }
+
+        if (args() != null) {
+            builder.addField("command.help.usage", argsAsString(command.args(), loc), false);
+        }
+
+        return builder.build();
+    }
+
+    private String argsAsString(SimpleArgument[] args, ContextLocalizer localizer) {
+        List<String> strArgs = new ArrayList<>();
+        for (var arg : args) {
+            strArgs.add(String.format(arg.isRequired() ? REQ : OPT, localizer.localize(arg.name())));
+        }
+        return String.join(" ", strArgs);
     }
 }
