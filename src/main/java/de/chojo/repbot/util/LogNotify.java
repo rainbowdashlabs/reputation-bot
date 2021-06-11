395d3514e5f9bd1ea1384b51c0767f81c47975d8
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-11 17:20:49 +0200

    Improve logging and add webhook logging

---
+++ src/main/java/de/chojo/repbot/util/LogNotify.java
@@ -0,0 +1,28 @@
+package de.chojo.repbot.util;
+
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+public class LogNotify {
+    /**
+     * Will be send to error-log channel.
+     */
+    public static final Marker NOTIFY_ADMIN = createMarker("NOTIFY_ADMIN");
+    /**
+     * Will be send to status-log.
+     */
+    public static final Marker STATUS = createMarker("STATUS");
+    /**
+     * Currently unused.
+     */
+    public static final Marker DISCORD = createMarker("DISCORD");
+
+    private static Marker createMarker(@NotNull String name, @NotNull Marker... children) {
+        var marker = MarkerFactory.getMarker(name);
+        for (var child : children) {
+            marker.add(child);
+        }
+        return marker;
+    }
+}
