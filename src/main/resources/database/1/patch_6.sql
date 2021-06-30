68fb921c7c9b55d29caebe55921573a101991adf
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-06-30 11:30:14 +0200

    Add more data to gdpr report

---
+++ src/main/resources/database/1/patch_6.sql
@@ -0,0 +1,81 @@
+alter table repbot_schema.gdpr_log
+    rename column tries to attemts;
+
+DROP FUNCTION repbot_schema.aggregate_user_data(bigint);
+
+CREATE OR REPLACE FUNCTION repbot_schema.aggregate_user_data(_user_id BIGINT)
+    RETURNS TEXT
+    LANGUAGE plpgsql
+    COST 100
+AS
+$BODY$
+DECLARE
+    _result JSONB;
+BEGIN
+    WITH reputation AS (
+        SELECT jsonb_agg(
+                       jsonb_build_object(
+                               'guild', guild_id,
+                               'channel', channel_id,
+                               'donor', CASE WHEN donor_id = _user_id THEN _user_id END,
+                               'receiver', CASE WHEN receiver_id = _user_id THEN _user_id END,
+                               'message', message_id,
+                               'ref_message', ref_message_id,
+                               'cause', cause,
+                               'received', received::TEXT
+                           )
+                   ) AS rep
+        FROM repbot_schema.reputation_log l
+        WHERE l.receiver_id = _user_id
+           OR l.donor_id = _user_id
+    ),
+         voice_activity AS (
+             SELECT jsonb_agg(
+                            jsonb_build_object(
+                                    'guild', guild_id,
+                                    'user_1', CASE WHEN user_id_1 = _user_id THEN _user_id END,
+                                    'user_2', CASE WHEN user_id_2 = _user_id THEN _user_id END,
+                                    'seen', seen::TEXT
+                                )
+                        ) AS voice
+             FROM repbot_schema.voice_activity
+             WHERE user_id_1 = _user_id
+                OR user_id_2 = _user_id
+         ),
+         cleanup_tasks as (
+             select jsonb_agg(
+                            jsonb_build_object(
+                                    'guild', guild_id,
+                                    'user', user_id,
+                                    'delete_after', delete_after::text
+                                )
+                        ) as cleanup
+             from repbot_schema.cleanup_schedule c
+             where c.user_id = _user_id
+         ),
+         gdpr_log as (
+             select jsonb_build_object(
+                            'user', user_id,
+                            'received', received,
+                            'attemts', attemts,
+                            'requested', requested
+                        ) as gdpr
+             from gdpr_log l
+             where l.user_id = _user_id
+         )
+    SELECT jsonb_pretty(
+                   jsonb_build_object(
+                           'reputation', coalesce(rep, '[]'::JSONB),
+                           'voice_activity', coalesce(voice, '[]'::JSONB),
+                           'cleanup_tasks', coalesce(cleanup, '[]'::jsonb),
+                           'gdpr_log', coalesce(gdpr_log, '{}'::jsonb)
+                       )
+               )
+    FROM reputation,
+         voice_activity,
+         cleanup_tasks,
+         gdpr_log
+    INTO _result;
+    RETURN _result;
+END;
+$BODY$;
