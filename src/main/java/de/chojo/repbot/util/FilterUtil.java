3ba41bb9efd6f1a42f8607bc25cf4656d855ea33
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-07-03 17:26:34 +0200

    Rework RepbotWrapper as general FilterUtil

--- src/main/java/de/chojo/repbot/util/FilterUtil.java
+++ src/main/java/de/chojo/repbot/util/FilterUtil.java
@@ -3,8 +3,13 @@ package de.chojo.repbot.data.wrapper;
 import de.chojo.repbot.data.GuildData;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.IPermissionHolder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RepBotWrapper {
 
@@ -36,6 +41,11 @@ public class RepBotWrapper {
                 guildData.addChannel(guild, textChannel);
             }
         }
+    }
+
+    public static List<TextChannel> filterChannelByPermission(Guild guild, Permission... permissions) {
+        var self = guild.getSelfMember();
+        return guild.getTextChannels().stream().filter(textChannel -> self.hasPermission(textChannel, permissions)).collect(Collectors.toList());
     }
 
     public static RepBotWrapper of(Member repBot, GuildData guildData) {
