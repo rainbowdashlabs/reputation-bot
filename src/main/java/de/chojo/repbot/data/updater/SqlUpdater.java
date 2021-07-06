1e0d5581e1140126568735307d6101807af752e4
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-07-06 13:12:24 +0200

    Bump db version

--- src/main/java/de/chojo/repbot/data/updater/SqlUpdater.java
+++ src/main/java/de/chojo/repbot/data/updater/SqlUpdater.java
@@ -15,7 +15,7 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class SqlUpdater {
     public static final int MAJOR = 1;
-    public static final int PATCH = 8;
+    public static final int PATCH = 9;
     private static final Logger log = getLogger(SqlUpdater.class);
     private final DataSource source;
     private final String versionTable;
