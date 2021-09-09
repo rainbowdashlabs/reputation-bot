5fff434d691b3864120f00f0ca334b489ab14d13
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-09-09 13:55:34 +0200

    Add unit tests for file consistency and locale files

---
+++ src/test/java/de/chojo/repbot/dataconsistency/Localization.java
@@ -0,0 +1,69 @@
+package de.chojo.repbot.dataconsistency;
+
+import de.chojo.jdautil.localization.util.Language;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+public class Localization {
+    public Language[] languages = {Language.ENGLISH, Language.GERMAN,
+            Language.of("es_ES", "Español"), Language.of("fr_FR", "Français"),
+            Language.of("ru_RU", "Russian")};
+    public Pattern replacements = Pattern.compile("%[a-zA-Z0-9.]+?%");
+
+    @Test
+    public void checkKeys() {
+        Map<Language, ResourceBundle> resourceBundles = new HashMap<>();
+        for (var code : languages) {
+            var locale = code.toLocale();
+            var bundle = ResourceBundle.getBundle("locale", locale);
+            resourceBundles.put(code, bundle);
+        }
+
+        System.out.printf("Loaded %s languages!%n", languages.length);
+
+        Set<String> keySet = new HashSet<>();
+        for (var resourceBundle : resourceBundles.values()) {
+            keySet.addAll(resourceBundle.keySet());
+        }
+
+        Map<String, Set<String>> replacements = new HashMap<>();
+        var english = resourceBundles.get(Language.ENGLISH);
+        for (var key : english.keySet()) {
+            replacements.put(key, getReplacements(english.getString(key)));
+            Assertions.assertFalse(english.getString(key).isBlank(), "Blank string at " + key + "@" + Language.ENGLISH);
+        }
+
+        for (var resourceBundle : resourceBundles.values()) {
+            for (var key : keySet) {
+                var id = key + "@" + resourceBundle.getLocale();
+                var locale = resourceBundle.getString(key);
+                Assertions.assertFalse(locale.isBlank(), "Blank or unlocalized key at " + id);
+                var localeReplacements = getReplacements(locale);
+                var defReplacements = replacements.get(key);
+                Assertions.assertTrue(localeReplacements.containsAll(defReplacements),
+                        "Missing replacement key in " + id
+                        + ". Expected \"" + String.join(", ", defReplacements) + "\". Actual \"" + String.join(", ", localeReplacements) + "\"");
+                System.out.printf("Checked %s%n", key);
+            }
+            System.out.printf("Checked Locale %s%n", resourceBundle.getLocale());
+        }
+    }
+
+    private Set<String> getReplacements(String message) {
+        Set<String> found = new HashSet<>();
+        var matcher = replacements.matcher(message);
+        while (matcher.find()) {
+            found.add(matcher.group());
+        }
+        return found;
+    }
+
+}
