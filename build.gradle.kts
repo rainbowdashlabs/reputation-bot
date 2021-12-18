564b561c8922147013a387b9db8f83aa48264ad5
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-12-18 23:15:39 +0100

    Bump log4j version to 2.17.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,6 @@ plugins {
 group = "de.chojo"
 version = "1.5.5"
 
-val log4jVersion = "2.16.0"
-
-
 repositories {
     maven("https://eldonexus.de/repository/maven-public")
     maven("https://eldonexus.de/repository/maven-proxies")
@@ -33,8 +30,8 @@ dependencies {
 
     // Logging
     implementation("org.slf4j", "slf4j-api", "1.7.30")
-    implementation("org.apache.logging.log4j", "log4j-core", log4jVersion)
-    implementation("org.apache.logging.log4j", "log4j-slf4j-impl", log4jVersion)
+    implementation("org.apache.logging.log4j", "log4j-core", "2.17.0")
+    implementation("org.apache.logging.log4j", "log4j-slf4j-impl", "2.17.0")
     implementation("club.minnced", "discord-webhooks", "0.5.8")
 
     // utils
