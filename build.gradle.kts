a0ec74ace99af5d9ceaeaaaa94d6593c4a95d4e8
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-04-14 10:47:45 +0200

    Implement empty page handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     //discord
-    implementation("de.chojo", "cjda-util", "2.3.1+alpha.9"){
+    implementation("de.chojo", "cjda-util", "2.3.3+alpha.9"){
         exclude(group = "club.minnced", module = "opus-java")
     }
 
