7ca7682356dfb3c3e98b22bed2a6a8f242e3886e
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-04-19 12:54:57 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     //discord
-    implementation("de.chojo", "cjda-util", "2.3.3+alpha.10-DEV"){
+    implementation("de.chojo", "cjda-util", "2.3.3+alpha.10"){
         exclude(group = "club.minnced", module = "opus-java")
     }
 
