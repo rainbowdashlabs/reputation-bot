f94ba540f8707e8794e5a5f0f319c723cf8bb75a
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-05-18 10:53:16 +0200

    Delegate user and guild retrieval from user gdpr to user

--- src/main/java/de/chojo/repbot/dao/access/guild/reputation/sub/user/Gdpr.java
+++ src/main/java/de/chojo/repbot/dao/access/guild/reputation/sub/user/Gdpr.java
@@ -3,7 +3,9 @@ package de.chojo.repbot.dao.access.guild.reputation.sub.user;
 import de.chojo.repbot.dao.access.guild.reputation.sub.RepUser;
 import de.chojo.repbot.dao.components.MemberHolder;
 import de.chojo.sqlutil.base.QueryFactoryHolder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 
 public class Gdpr extends QueryFactoryHolder implements MemberHolder {
     private final RepUser repUser;
@@ -41,5 +43,15 @@ public class Gdpr extends QueryFactoryHolder implements MemberHolder {
                         """)
                 .paramsBuilder(stmt -> stmt.setLong(guildId()).setLong(userId()))
                 .update().executeSync();
+    }
+
+    @Override
+    public User user() {
+        return repUser.user();
+    }
+
+    @Override
+    public Guild guild() {
+        return repUser.guild();
     }
 }
