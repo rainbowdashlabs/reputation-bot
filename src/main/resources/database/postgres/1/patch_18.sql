25612c4a4ee5d7a04f1730066501782855c540b9
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-06-11 02:44:21 +0200

    Remove null entried from reputation bot and fix the donor count on week and month view

---
+++ src/main/resources/database/postgres/1/patch_18.sql
@@ -0,0 +1,121 @@
+DROP VIEW repbot_schema.user_reputation_week;
+CREATE OR REPLACE VIEW repbot_schema.user_reputation_week(rank, rank_donated, guild_id, user_id, reputation, donated) AS
+SELECT ROW_NUMBER() OVER (PARTITION BY rank.guild_id ORDER BY rank.reputation DESC) AS rank,
+       ROW_NUMBER() OVER (PARTITION BY rank.guild_id ORDER BY rank.donated DESC)    AS rank_donated,
+       rank.guild_id,
+       rank.user_id,
+       rank.reputation,
+       rank.donated
+FROM (SELECT log.guild_id,
+             log.user_id,
+             log.reputation,
+             log.donated
+      FROM (SELECT COALESCE(don.guild_id, rep.guild_id)    AS guild_id,
+                   COALESCE(rep.receiver_id, don.donor_id) AS user_id,
+                   COALESCE(rep.reputation, 0::BIGINT)     AS reputation,
+                   COALESCE(don.donated, 0::BIGINT)        AS donated
+            FROM (SELECT r.guild_id # r.receiver_id AS key,
+                         r.guild_id,
+                         r.receiver_id,
+                         COUNT(1)                   AS reputation
+                  FROM repbot_schema.reputation_log r
+                  WHERE r.received > NOW() - INTERVAL '1 week'
+                  GROUP BY r.guild_id, r.receiver_id) rep
+                     FULL JOIN (SELECT r.guild_id # r.donor_id AS key,
+                                       r.guild_id,
+                                       r.donor_id,
+                                       COUNT(1)                AS donated
+                                FROM repbot_schema.reputation_log r
+                                WHERE r.received > NOW() - INTERVAL '1 week'
+                                GROUP BY r.guild_id, r.donor_id) don ON rep.key = don.key) log
+      WHERE NOT (EXISTS(SELECT 1
+                        FROM repbot_schema.cleanup_schedule clean
+                        WHERE clean.guild_id = log.guild_id
+                          AND clean.user_id = log.user_id))
+        AND user_id IS NOT NULL) rank;
+
+DROP VIEW repbot_schema.user_reputation_month;
+CREATE OR REPLACE VIEW repbot_schema.user_reputation_month(rank, rank_donated, guild_id, user_id, reputation, donated) AS
+SELECT ROW_NUMBER() OVER (PARTITION BY rank.guild_id ORDER BY rank.reputation DESC) AS rank,
+       ROW_NUMBER() OVER (PARTITION BY rank.guild_id ORDER BY rank.donated DESC)    AS rank_donated,
+       rank.guild_id,
+       rank.user_id,
+       rank.reputation,
+       rank.donated
+FROM (SELECT log.guild_id,
+             log.user_id,
+             log.reputation,
+             log.donated
+      FROM (SELECT COALESCE(don.guild_id, rep.guild_id)    AS guild_id,
+                   COALESCE(rep.receiver_id, don.donor_id) AS user_id,
+                   COALESCE(rep.reputation, 0::BIGINT)     AS reputation,
+                   COALESCE(don.donated, 0::BIGINT)        AS donated
+            FROM (SELECT r.guild_id # r.receiver_id AS key,
+                         r.guild_id,
+                         r.receiver_id,
+                         COUNT(1)                   AS reputation
+                  FROM repbot_schema.reputation_log r
+                  WHERE r.received > NOW() - INTERVAL '4 week'
+                  GROUP BY r.guild_id, r.receiver_id) rep
+                     FULL JOIN (SELECT r.guild_id # r.donor_id AS key,
+                                       r.guild_id,
+                                       r.donor_id,
+                                       COUNT(1)                AS donated
+                                FROM repbot_schema.reputation_log r
+                                WHERE r.received > NOW() - INTERVAL '4 week'
+                                GROUP BY r.guild_id, r.donor_id) don ON rep.key = don.key) log
+      WHERE NOT (EXISTS(SELECT 1
+                        FROM repbot_schema.cleanup_schedule clean
+                        WHERE clean.guild_id = log.guild_id
+                          AND clean.user_id = log.user_id))
+        AND user_id IS NOT NULL) rank;
+
+DROP VIEW repbot_schema.global_user_reputation;
+DROP VIEW repbot_schema.user_reputation;
+CREATE OR REPLACE VIEW repbot_schema.user_reputation(rank, rank_donated, guild_id, user_id, reputation, donated) AS
+SELECT ROW_NUMBER() OVER (PARTITION BY rank.guild_id ORDER BY rank.reputation DESC) AS rank,
+       ROW_NUMBER() OVER (PARTITION BY rank.guild_id ORDER BY rank.donated DESC)    AS rank_donated,
+       rank.guild_id,
+       rank.user_id,
+       rank.reputation,
+       rank.donated
+FROM (SELECT guild_id,
+             user_id,
+             reputation,
+             donated
+      FROM (SELECT COALESCE(don.guild_id, rep.guild_id)    AS guild_id,
+                   COALESCE(rep.receiver_id, don.donor_id) AS user_id,
+                   COALESCE(rep.reputation, 0::BIGINT)     AS reputation,
+                   COALESCE(don.donated, 0::BIGINT)        AS donated
+            FROM (SELECT r.guild_id # r.receiver_id AS key,
+                         r.guild_id,
+                         r.receiver_id,
+                         COUNT(1)                   AS reputation
+                  FROM repbot_schema.reputation_log r
+                  GROUP BY r.guild_id, r.receiver_id) rep
+                     FULL JOIN (SELECT r.guild_id # r.donor_id AS key,
+                                       r.guild_id,
+                                       r.donor_id,
+                                       COUNT(1)                AS donated
+                                FROM repbot_schema.reputation_log r
+                                GROUP BY r.guild_id, r.donor_id) don
+                               ON rep.key = don.key) log
+      WHERE NOT EXISTS(SELECT 1
+                       FROM repbot_schema.cleanup_schedule clean
+                       WHERE clean.guild_id = log.guild_id
+                         AND clean.user_id = log.user_id)
+        AND user_id IS NOT NULL) rank;
+
+CREATE OR REPLACE VIEW repbot_schema.global_user_reputation(rank, rank_donated, user_id, reputation, donated) AS
+SELECT ROW_NUMBER() OVER (ORDER BY reputation DESC) AS rank,
+       ROW_NUMBER() OVER (ORDER BY donated DESC)    AS rank_donated,
+       user_id,
+       reputation,
+       donated
+FROM (SELECT user_reputation.user_id,
+             SUM(user_reputation.reputation) AS reputation,
+             SUM(user_reputation.donated)    AS donated
+      FROM repbot_schema.user_reputation
+      GROUP BY user_reputation.user_id) rep;
+
+UPDATE repbot_schema.reputation_log SET donor_id = NULL WHERE donor_id = 0;
