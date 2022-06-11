c2d4c183a40187a593a270cb7b8959007199f7f8
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-06-11 15:42:26 +0200

    Integrate categories into channel command

--- src/main/java/de/chojo/repbot/commands/Channel.java
+++ src/main/java/de/chojo/repbot/commands/Channel.java
@@ -3,14 +3,16 @@ package de.chojo.repbot.commands;
 import de.chojo.jdautil.command.CommandMeta;
 import de.chojo.jdautil.command.SimpleArgument;
 import de.chojo.jdautil.command.SimpleCommand;
+import de.chojo.jdautil.localization.util.LocalizedEmbedBuilder;
 import de.chojo.jdautil.localization.util.Replacement;
 import de.chojo.jdautil.util.Completion;
 import de.chojo.jdautil.wrapper.SlashCommandContext;
 import de.chojo.repbot.dao.access.guild.settings.sub.thanking.Channels;
 import de.chojo.repbot.dao.provider.Guilds;
-import de.chojo.repbot.util.FilterUtil;
+import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.IMentionable;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
@@ -22,11 +24,11 @@ public class Channel extends SimpleCommand {
     public Channel(Guilds guilds) {
         super(CommandMeta.builder("channel", "command.channel.description")
                 .addSubCommand("set", "command.channel.sub.set", argsBuilder()
-                        .add(SimpleArgument.channel("channel", "command.channel.sub.set.arg.channel")))
+                        .add(SimpleArgument.channel("channel", "command.channel.sub.set.arg.channel").asRequired()))
                 .addSubCommand("add", "command.channel.sub.add", argsBuilder()
-                        .add(SimpleArgument.channel("channel", "command.channel.sub.add.arg.channel")))
+                        .add(SimpleArgument.channel("channel", "command.channel.sub.add.arg.channel").asRequired()))
                 .addSubCommand("remove", "command.channel.sub.remove", argsBuilder()
-                        .add(SimpleArgument.channel("channel", "command.channel.sub.remove.arg.channel")))
+                        .add(SimpleArgument.channel("channel", "command.channel.sub.remove.arg.channel").asRequired()))
                 .addSubCommand("list_type", "command.channel.sub.listType", argsBuilder()
                         .add(SimpleArgument.string("type", "command.channel.sub.listType.arg.type").withAutoComplete()))
                 .addSubCommand("list", "command.channel.sub.list")
@@ -66,51 +68,93 @@ public class Channel extends SimpleCommand {
     }
 
     private void add(SlashCommandInteractionEvent event, SlashCommandContext context, Channels channels) {
-        var channel = event.getOption("channel").getAsMessageChannel();
-        if (channel == null || channel.getType() != ChannelType.TEXT) {
-            event.reply(context.localize("error.invalidChannel")).setEphemeral(true).queue();
+        var channelType = event.getOption("channel").getChannelType();
+        if (channelType != ChannelType.TEXT && channelType != ChannelType.CATEGORY) {
+            event.reply(context.localize("error.onlyTextOrCategory")).setEphemeral(true).queue();
             return;
         }
-        channels.add(channel);
-        event.reply(
-                context.localize("command.channel.sub.add.added",
-                        Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        if (channelType == ChannelType.TEXT) {
+            var channel = event.getOption("channel").getAsTextChannel();
+            channels.add(channel);
+            event.reply(
+                    context.localize("command.channel.sub.add.added",
+                            Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        } else {
+            var channel = event.getOption("channel").getAsGuildChannel();
+            channels.add((Category) channel);
+            event.reply(
+                    context.localize("command.channel.sub.add.added",
+                            Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        }
     }
 
     private void remove(SlashCommandInteractionEvent event, SlashCommandContext context, Channels channels) {
-        var channel = event.getOption("channel").getAsMessageChannel();
-        if (channel == null || channel.getType() != ChannelType.TEXT) {
-            event.reply(context.localize("error.invalidChannel")).setEphemeral(true).queue();
+        var channelType = event.getOption("channel").getChannelType();
+        if (channelType != ChannelType.TEXT && channelType != ChannelType.CATEGORY) {
+            event.reply(context.localize("error.onlyTextOrCategory")).setEphemeral(true).queue();
             return;
         }
-        channels.remove(channel);
-
-        event.reply(context.localize("command.channel.sub.remove.removed",
-                Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        if (channelType == ChannelType.TEXT) {
+            var channel = event.getOption("channel").getAsTextChannel();
+            channels.remove(channel);
+            event.reply(
+                    context.localize("command.channel.sub.remove.removed",
+                            Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        } else {
+            var channel = event.getOption("channel").getAsGuildChannel();
+            channels.remove((Category) channel);
+            event.reply(
+                    context.localize("command.channel.sub.remove.removed",
+                            Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        }
     }
 
     private void list(SlashCommandInteractionEvent event, SlashCommandContext context, Channels channels) {
-        event.reply(getChannelList(channels, context)).queue();
+        event.replyEmbeds(getChannelList(channels, context)).queue();
     }
 
-    private String getChannelList(Channels channels, SlashCommandContext context) {
-        var channelNames = channels.channels().stream().map(IMentionable::getAsMention).collect(Collectors.joining(", "));
-        var message = "command.channel.sub.list." + (channels.isWhitelist() ? "whitelist" : "blacklist");
-        return context.localize(message, Replacement.create("CHANNEL", channelNames));
+    private MessageEmbed getChannelList(Channels channels, SlashCommandContext context) {
+
+        var channelNames = channels.channels().stream().map(IMentionable::getAsMention).limit(40).collect(Collectors.joining(", "));
+        if (channels.channels().size() > 40) {
+            channelNames += String.format("$%s$", "command.channel.list.more");
+        }
+
+        var categoryNames = channels.categories().stream().map(IMentionable::getAsMention).limit(40).collect(Collectors.joining(", "));
+        if (channels.categories().size() > 40) {
+            categoryNames += String.format("$%s$", "command.channel.list.more");
+        }
+
+        return new LocalizedEmbedBuilder(context.localizer())
+                .setTitle(channels.isWhitelist() ? "command.channel.sub.list.whitelist" : "command.channel.sub.list.blacklist")
+                .addField("words.channels", channelNames, false, Replacement.create("MORE", channels.channels().size() - 40))
+                .addField("words.category", categoryNames, false, Replacement.create("MORE", channels.channels().size() - 40))
+                .build();
     }
 
     private void set(SlashCommandInteractionEvent event, SlashCommandContext context, Channels channels) {
-        var channel = event.getOption("channel").getAsMessageChannel();
-        if (channel == null || channel.getType() != ChannelType.TEXT) {
-            event.reply(context.localize("error.invalidChannel")).setEphemeral(true).queue();
+        var channelType = event.getOption("channel").getChannelType();
+        if (channelType != ChannelType.TEXT && channelType != ChannelType.CATEGORY) {
+            event.reply(context.localize("error.onlyTextOrCategory")).setEphemeral(true).queue();
             return;
         }
 
         event.deferReply().queue();
-        channels.clearChannel();
-        channels.add(channel);
-        event.getHook().editOriginal(context.localize("command.channel.sub.set.set",
-                Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        if (channelType == ChannelType.TEXT) {
+            var channel = event.getOption("channel").getAsTextChannel();
+            channels.clearChannel();
+            channels.add(channel);
+            event.getHook().editOriginal(
+                    context.localize("command.channel.sub.set.set",
+                            Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        } else {
+            var channel = event.getOption("channel").getAsGuildChannel();
+            channels.clearCategories();
+            channels.add((Category) channel);
+            event.getHook().editOriginal(
+                    context.localize("command.channel.sub.set.set",
+                            Replacement.create("CHANNEL", channel.getAsMention()))).queue();
+        }
     }
 
     @Override
