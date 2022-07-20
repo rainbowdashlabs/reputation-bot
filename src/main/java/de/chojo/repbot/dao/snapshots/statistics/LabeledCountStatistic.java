144e487447e35a0a9b42b2b5c37cb83df14e090f
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-07-20 11:47:34 +0200

    Apply suggestions from code review
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>

--- src/main/java/de/chojo/repbot/dao/snapshots/statistics/LabeledCountStatistic.java
+++ src/main/java/de/chojo/repbot/dao/snapshots/statistics/LabeledCountStatistic.java
@@ -40,8 +40,6 @@ public record LabeledCountStatistic(Map<String, List<CountStatistics>> stats) im
                     .setMarker(SeriesMarkers.NONE)
                     .setLabel(entry.getKey());
         }
-
-
         try {
             return BitmapEncoder.getBitmapBytes(categorySeries, BitmapEncoder.BitmapFormat.PNG);
         } catch (IOException e) {
