e3406cb40a20ca838540dafc6ee740a4d563b378
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-13 17:27:39 +0200

    migrate repadmin command (#362)
    * migrate repadmin command
    
    * Update src/main/java/de/chojo/repbot/commands/repadmin/handler/Profile.java
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>

---
+++ src/main/java/de/chojo/repbot/commands/repadmin/handler/Profile.java
@@ -0,0 +1,24 @@
+package de.chojo.repbot.commands.repadmin.handler;
+
+import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
+import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.config.Configuration;
+import de.chojo.repbot.dao.provider.Guilds;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class Profile implements SlashHandler {
+    private final Guilds guilds;
+    private final Configuration configuration;
+
+    public Profile(Guilds guilds, Configuration configuration) {
+        this.guilds = guilds;
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
+        var user = guilds.guild(event.getGuild()).reputation().user(event.getOption("user").getAsMember());
+        var profile = user.profile().adminProfile(configuration, context.guildLocalizer());
+        event.replyEmbeds(profile).setEphemeral(true).queue();
+    }
+}
---
+++ src/main/java/de/chojo/repbot/commands/repadmin/handler/reputation/Add.java
@@ -0,0 +1,26 @@
+package de.chojo.repbot.commands.repadmin.handler.reputation;
+
+import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
+import de.chojo.jdautil.localization.util.Replacement;
+import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.dao.provider.Guilds;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class Add implements SlashHandler {
+    private final Guilds guilds;
+
+    public Add(Guilds guilds) {
+        this.guilds = guilds;
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
+        var user = event.getOption("user").getAsMember();
+        var repUser = guilds.guild(event.getGuild()).reputation().user(user);
+        var add = event.getOption("add").getAsLong();
+        repUser.addReputation(add);
+        event.reply(context.localize("command.repadmin.reputation.add.message.added",
+                        Replacement.create("VALUE", add), Replacement.createMention(user)))
+                .setEphemeral(true).queue();
+    }
+}
