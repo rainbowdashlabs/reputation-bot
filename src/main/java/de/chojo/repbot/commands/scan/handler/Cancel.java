29e348834a1b9156ab68c74336553716c22ccfb8
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-13 15:12:14 +0200

    Migrate scan command (#367)

---
+++ src/main/java/de/chojo/repbot/commands/scan/handler/Cancel.java
@@ -0,0 +1,24 @@
+package de.chojo.repbot.commands.scan.handler;
+
+import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
+import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.commands.scan.util.Scanner;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class Cancel implements SlashHandler {
+    private final Scanner scanner;
+
+    public Cancel(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
+        if (!scanner.isActive(event.getGuild())) {
+            event.reply(context.localize("command.scan.cancel.message.notask")).setEphemeral(true).queue();
+            return;
+        }
+        event.reply(context.localize("command.scan.cancel.message.canceling")).queue();
+        scanner.cancelScan(event.getGuild());
+    }
+}
