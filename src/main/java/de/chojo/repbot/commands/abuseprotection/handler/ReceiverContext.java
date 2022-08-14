8f38c0d899ec4a4423f2dc1fd3d100134ed8547e
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-14 18:23:19 +0200

    Migrate abuse protection (#332)
    * Migrate abuse protection
    
    * Adjust locale key names
    
    * Adjust locale key names
    
    * Update src/main/java/de/chojo/repbot/commands/abuseprotection/handler/Info.java
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>

---
+++ src/main/java/de/chojo/repbot/commands/abuseprotection/handler/ReceiverContext.java
@@ -0,0 +1,30 @@
+package de.chojo.repbot.commands.abuseprotection.handler;
+
+import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
+import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.dao.access.guild.RepGuild;
+import de.chojo.repbot.dao.provider.Guilds;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class ReceiverContext implements SlashHandler, BooleanMessageMapper {
+    private final Guilds guilds;
+
+    public ReceiverContext(Guilds guilds) {
+        this.guilds = guilds;
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
+        RepGuild guild = guilds.guild(event.getGuild());
+        var abuseSettings = guild.settings().abuseProtection();
+        if (event.getOptions().isEmpty()) {
+            event.reply(getBooleanMessage(context, abuseSettings.isReceiverContext(),
+                    "command.abuseprotection.receivercontext.message.true", "command.abuseprotection.receivercontext.message.false")).queue();
+            return;
+        }
+        var state = event.getOption("state").getAsBoolean();
+
+        event.reply(getBooleanMessage(context, abuseSettings.receiverContext(state),
+                "command.abuseprotection.receivercontext.message.true", "command.abuseprotection.receivercontext.message.false")).queue();
+    }
+}
