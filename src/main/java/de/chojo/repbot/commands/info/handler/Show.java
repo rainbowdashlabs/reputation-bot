df77d8cfdaba5e289f449e8cd5ff41c17df98be6
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-14 10:37:56 +0200

    Migrate info command (#354)
    * Migrate info command
    
    * Adjust locale key names

--- src/main/java/de/chojo/repbot/commands/info/handler/Show.java
+++ src/main/java/de/chojo/repbot/commands/info/handler/Show.java
@@ -1,15 +1,14 @@
-package de.chojo.repbot.commands;
+package de.chojo.repbot.commands.info.handler;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import de.chojo.jdautil.command.CommandMeta;
-import de.chojo.jdautil.command.SimpleCommand;
+import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
 import de.chojo.jdautil.localization.util.LocalizedEmbedBuilder;
 import de.chojo.jdautil.localization.util.Replacement;
-import de.chojo.jdautil.wrapper.SlashCommandContext;
+import de.chojo.jdautil.wrapper.EventContext;
 import de.chojo.repbot.config.Configuration;
 import de.chojo.repbot.util.Colors;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -23,7 +22,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -33,44 +31,31 @@ import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-public class Info extends SimpleCommand {
+public class Show implements SlashHandler {
     private static final String ART = "**SmartieFox ☆*:.｡.o(≧▽≦)o.｡.:*☆**\n[Twitter](https://twitter.com/smartiefoxart) [Twitch](https://www.twitch.tv/smartiefox)";
     private static final String SOURCE = "[rainbowdashlabs/reputation-bot](https://github.com/rainbowdashlabs/reputation-bot)";
-    private static final Logger log = getLogger(Info.class);
+    private static final Logger log = getLogger(Show.class);
     private final HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build();
     private final ObjectMapper mapper = new ObjectMapper()
             .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    private final String version;
-    private final Configuration configuration;
     private String contributors;
     private Instant lastFetch = Instant.MIN;
+    private final String version;
+    private final Configuration configuration;
 
-
-    public Info(String version, Configuration configuration) {
-        super(CommandMeta.builder("info", "command.info.description"));
+    public Show(String version, Configuration configuration) {
         this.version = version;
         this.configuration = configuration;
     }
 
-    public static Info create(Configuration configuration) {
-        var version = "undefined";
-        try (var input = Info.class.getClassLoader().getResourceAsStream("version")) {
-            version = new String(input.readAllBytes(), StandardCharsets.UTF_8).trim();
-        } catch (IOException e) {
-            log.error("Could not determine version.");
-        }
-        return new Info(version, configuration);
-    }
-
-
     @Override
-    public void onSlashCommand(SlashCommandInteractionEvent event, SlashCommandContext context) {
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
         event.replyEmbeds(getResponse(event, context)).queue();
     }
 
     @NotNull
-    private MessageEmbed getResponse(SlashCommandInteractionEvent event, SlashCommandContext context) {
+    private MessageEmbed getResponse(SlashCommandInteractionEvent event, EventContext context) {
         if (contributors == null || lastFetch.isBefore(Instant.now().minus(5, ChronoUnit.MINUTES))) {
             var request = HttpRequest.newBuilder().GET()
                     .uri(URI.create("https://api.github.com/repos/rainbowdashlabs/reputation-bot/contributors?anon=1"))
@@ -108,42 +93,42 @@ public class Info extends SimpleCommand {
             lastFetch = Instant.now();
         }
 
-        return new LocalizedEmbedBuilder(context.localizer())
-                .setTitle("command.info.embedTitle")
+        return new LocalizedEmbedBuilder(context.guildLocalizer())
+                .setTitle("command.info.message.title")
                 .setThumbnail(event.getJDA().getSelfUser().getEffectiveAvatarUrl())
-                .addField("command.info.contributor", contributors, false)
-                .addField("command.info.art", ART, true)
-                .addField("command.info.source", SOURCE, true)
-                .addField("command.info.version", version, true)
-                .addField("command.info.supportMe", getVoting(context), true)
+                .addField("command.info.message.contributor", contributors, false)
+                .addField("command.info.message.art", ART, true)
+                .addField("command.info.message.source", SOURCE, true)
+                .addField("command.info.message.version", version, true)
+                .addField("command.info.message.supportMe", getVoting(context), true)
                 .addField("", "**" + getLinks(context) + "**", false)
                 .setColor(Colors.Pastel.BLUE)
                 .build();
     }
 
-    private String getLinks(SlashCommandContext context) {
+    private String getLinks(EventContext context) {
         var links = List.of(
-                getLink(context, "command.info.inviteMe", configuration.links().invite()),
-                getLink(context, "command.info.support", configuration.links().support()),
-                getLink(context, "command.info.tos", configuration.links().tos()),
-                getLink(context, "command.info.website", configuration.links().website()),
-                getLink(context, "command.info.faq", configuration.links().faq())
+                getLink(context, "command.info.message.inviteme", configuration.links().invite()),
+                getLink(context, "command.info.message.support", configuration.links().support()),
+                getLink(context, "command.info.message.tos", configuration.links().tos()),
+                getLink(context, "command.info.message.website", configuration.links().website()),
+                getLink(context, "command.info.message.faq", configuration.links().faq())
         );
         return String.join(" ᠅ ", links);
     }
 
-    private String getLink(SlashCommandContext context, @PropertyKey(resourceBundle = "locale") String target, String url) {
+    private String getLink(EventContext context, @PropertyKey(resourceBundle = "locale") String target, String url) {
         return context.localize("words.link", Replacement.create("TARGET", String.format("$%s$", target)),
                 Replacement.create("URL", url));
     }
 
-    private String getUntranslatedLink(SlashCommandContext context, String target, String url) {
+    private String getUntranslatedLink(EventContext context, String target, String url) {
         return context.localize("words.link",
                 Replacement.create("TARGET", target),
                 Replacement.create("URL", url));
     }
 
-    private String getVoting(SlashCommandContext context) {
+    private String getVoting(EventContext context) {
         List<String> voteLinks = new ArrayList<>();
 
         for (var botlist : configuration.botlist().botlists()) {
