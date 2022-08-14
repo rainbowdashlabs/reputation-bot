676768802aa07bfb3022c002b33684556a648b9f
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-14 17:36:55 +0200

    migrate reactions command (#361)
    * migrate reactions command
    
    * Update ReputationBot.java
    
    * Update ReputationBot.java
    
    * Apply suggestions from code review
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    
    * Update src/main/java/de/chojo/repbot/commands/reactions/handler/Add.java
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>
    
    Co-authored-by: Niklas van Schrick <mc.taucher2003@gmail.com>

---
+++ src/main/java/de/chojo/repbot/commands/reactions/handler/Info.java
@@ -0,0 +1,35 @@
+package de.chojo.repbot.commands.reactions.handler;
+
+import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
+import de.chojo.jdautil.localization.util.LocalizedEmbedBuilder;
+import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.dao.access.guild.settings.Settings;
+import de.chojo.repbot.dao.provider.Guilds;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class Info implements SlashHandler {
+    private final Guilds guilds;
+
+    public Info(Guilds guilds) {
+        this.guilds = guilds;
+    }
+
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
+        event.replyEmbeds(getInfoEmbed(guilds.guild(event.getGuild()).settings(), context)).queue();
+    }
+
+    private MessageEmbed getInfoEmbed(Settings settings, EventContext context) {
+        var reactions = settings.thanking().reactions();
+        var mainEmote = reactions.reactionMention();
+        var emotes = String.join(" ", reactions.getAdditionalReactionMentions());
+
+        return new LocalizedEmbedBuilder(context.guildLocalizer())
+                .setTitle("command.reaction.info.message.title")
+                .addField("command.reaction.info.message.main", mainEmote.orElse("words.unknown"), true)
+                .addField("command.reaction.info.message.additional", emotes, true)
+                .build();
+    }
+
+}
---
+++ src/main/java/de/chojo/repbot/commands/reactions/util/EmojiCheckResult.java
@@ -0,0 +1,4 @@
+package de.chojo.repbot.commands.reactions.util;
+
+public record EmojiCheckResult(String mention, String id, CheckResult result) {
+}
