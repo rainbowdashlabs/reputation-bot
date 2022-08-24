fd3a75754746a11951bae65d15f5bccba8d9c9ad
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-08-24 11:23:22 +0200

    Split ReputationBot class into multiple classes

---
+++ src/main/java/de/chojo/repbot/core/Data.java
@@ -0,0 +1,123 @@
+package de.chojo.repbot.core;
+
+import com.zaxxer.hikari.HikariDataSource;
+import de.chojo.repbot.config.Configuration;
+import de.chojo.repbot.dao.access.Cleanup;
+import de.chojo.repbot.dao.access.Gdpr;
+import de.chojo.repbot.dao.provider.Guilds;
+import de.chojo.repbot.dao.provider.Metrics;
+import de.chojo.repbot.dao.provider.Voice;
+import de.chojo.repbot.util.LogNotify;
+import de.chojo.sqlutil.databases.SqlType;
+import de.chojo.sqlutil.datasource.DataSourceCreator;
+import de.chojo.sqlutil.updater.QueryReplacement;
+import de.chojo.sqlutil.updater.SqlUpdater;
+import de.chojo.sqlutil.wrapper.QueryBuilderConfig;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class Data {
+    private static final Logger log = getLogger(Data.class);
+    private final Threading threading;
+    private final Configuration configuration;
+    private HikariDataSource dataSource;
+    private Guilds guilds;
+    private Gdpr gdpr;
+    private Cleanup cleanup;
+    private Metrics metrics;
+    private Voice voice;
+
+    private Data(Threading threading, Configuration configuration) {
+        this.threading = threading;
+        this.configuration = configuration;
+    }
+
+    public static Data create(Threading threading, Configuration configuration) throws SQLException, IOException {
+        var data = new Data(threading, configuration);
+        data.init();
+        return data;
+    }
+
+    public void init() throws SQLException, IOException {
+        configure();
+        initConnection();
+        updateDatabase();
+        initDao();
+    }
+
+    public void initConnection() {
+        dataSource = getConnectionPool();
+    }
+
+    private void updateDatabase() throws IOException, SQLException {
+        var schema = configuration.database().schema();
+        SqlUpdater.builder(dataSource, SqlType.POSTGRES)
+                .setReplacements(new QueryReplacement("repbot_schema", schema))
+                .setVersionTable(schema + ".repbot_version")
+                .setSchemas(schema)
+                .execute();
+    }
+
+    private void configure() {
+        log.info("Configuring QueryBuilder");
+        var logger = getLogger("DbLogger");
+        QueryBuilderConfig.setDefault(QueryBuilderConfig.builder()
+                .withExceptionHandler(err -> logger.error(LogNotify.NOTIFY_ADMIN, "An error occured during a database request", err))
+                .withExecutor(threading.repBotWorker())
+                .build());
+    }
+
+    private void initDao() {
+        log.info("Creating DAOs");
+        guilds = new Guilds(dataSource);
+        gdpr = new Gdpr(dataSource);
+        cleanup = new Cleanup(dataSource);
+        metrics = new Metrics(dataSource);
+        voice = new Voice(dataSource);
+    }
+
+    private HikariDataSource getConnectionPool() {
+        log.info("Creating connection pool.");
+        var data = configuration.database();
+        return DataSourceCreator.create(SqlType.POSTGRES)
+                .configure(config -> config
+                        .host(data.host())
+                        .port(data.port())
+                        .user(data.user())
+                        .password(data.password())
+                        .database(data.database()))
+                .create()
+                .withMaximumPoolSize(data.poolSize())
+                .withThreadFactory(Threading.createThreadFactory(threading.hikariGroup()))
+                .forSchema(data.schema())
+                .build();
+    }
+
+    public Guilds guilds() {
+        return guilds;
+    }
+
+    public Gdpr gdpr() {
+        return gdpr;
+    }
+
+    public Cleanup cleanup() {
+        return cleanup;
+    }
+
+    public Metrics metrics() {
+        return metrics;
+    }
+
+    public Voice voice() {
+        return voice;
+    }
+
+    public void shutDown() {
+        dataSource.close();
+    }
+}
---
+++ src/main/java/de/chojo/repbot/core/Shutdown.java
@@ -0,0 +1,40 @@
+package de.chojo.repbot.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class Shutdown {
+    private static final Logger log = getLogger(Shutdown.class);
+    private final Bot bot;
+    private final Threading threading;
+    private final Data data;
+
+    private Shutdown(Bot bot, Threading threading, Data data) {
+        this.bot = bot;
+        this.threading = threading;
+        this.data = data;
+    }
+
+    public static Shutdown create(Bot bot, Threading threading, Data data) {
+        var shutdown = new Shutdown(bot, threading, data);
+        shutdown.init();
+        return shutdown;
+    }
+
+    public void init() {
+        log.info("Creating Shutdown Hook");
+        var shutdown = new Thread(() -> {
+            log.info("Shuting down shardmanager.");
+            bot.shutdown();
+            log.info("Shutting down scheduler.");
+            threading.shutdown();
+            log.info("Shutting down database connections.");
+            data.shutDown();
+            log.info("Bot shutdown complete.");
+            LogManager.shutdown();
+        });
+        Runtime.getRuntime().addShutdownHook(shutdown);
+    }
+}
