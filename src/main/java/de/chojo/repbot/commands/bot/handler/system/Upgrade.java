118a211d8f151c4e46787f9d55ff0d51f2c06193
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-09-27 15:45:27 +0200

    Cleanup

--- src/main/java/de/chojo/repbot/commands/bot/handler/system/Upgrade.java
+++ src/main/java/de/chojo/repbot/commands/bot/handler/system/Upgrade.java
@@ -2,6 +2,7 @@ package de.chojo.repbot.commands.bot.handler.system;
 
 import de.chojo.jdautil.interactions.slash.structure.handler.SlashHandler;
 import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.config.Configuration;
 import de.chojo.repbot.util.LogNotify;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import org.slf4j.Logger;
@@ -10,8 +11,18 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 public class Upgrade implements SlashHandler {
     private static final Logger log = getLogger(Upgrade.class);
+    private final Configuration configuration;
+
+    public Upgrade(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @Override
     public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
+        if (!configuration.baseSettings().isOwner(event.getUser().getIdLong())) {
+            event.reply("No.").setEphemeral(true).queue();
+            return;
+        }
         log.info(LogNotify.STATUS, "Upgrade command received from {}. Attempting upgrade.", event.getUser().getAsTag());
         event.reply("Starting upgrade. Will be back soon!").complete();
         System.exit(20);
