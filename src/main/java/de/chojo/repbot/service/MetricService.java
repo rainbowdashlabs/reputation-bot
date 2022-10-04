4f37c07a198d0cb05da87d68998590807b0d4a76
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-10-04 16:50:40 +0200

    Remove unused imports

--- src/main/java/de/chojo/repbot/service/MetricService.java
+++ src/main/java/de/chojo/repbot/service/MetricService.java
@@ -3,12 +3,9 @@ package de.chojo.repbot.service;
 import de.chojo.repbot.dao.provider.Metrics;
 
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.TimeZone;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -21,7 +18,8 @@ public class MetricService implements Runnable {
 
     public static MetricService create(ScheduledExecutorService executorService, Metrics metrics) {
         var now = ZonedDateTime.now(ZoneOffset.UTC);
-        var midnight = now.toLocalDate().atStartOfDay().plus(1, ChronoUnit.DAYS).plus(1, ChronoUnit.HOURS).atOffset(ZoneOffset.UTC);
+        var midnight = now.toLocalDate().atStartOfDay().plus(1, ChronoUnit.DAYS).plus(1, ChronoUnit.HOURS)
+                          .atOffset(ZoneOffset.UTC);
         var minutes = now.until(midnight, ChronoUnit.MINUTES);
         var service = new MetricService(metrics);
         executorService.scheduleAtFixedRate(service, minutes, 1440, TimeUnit.MINUTES);
