279a373be05c8e77af7c0934fdd60670d79955fa
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-10-06 14:34:29 +0200

    Revert changes on patch 23

--- src/main/resources/database/postgresql/1/patch_23.sql
+++ src/main/resources/database/postgresql/1/patch_23.sql
@@ -1,85 +1,50 @@
 -- feature/guild-cleanup
 CREATE OR REPLACE VIEW repbot_schema.guilds AS
-SELECT guild_id
-FROM repbot_schema.reputation_log
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.abuse_protection
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.active_categories
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.active_channel
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.announcements
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.donor_roles
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.guild_ranks
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.guild_reactions
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.guild_settings
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.message_states
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.receiver_roles
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.donor_roles
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.reputation_offset
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.reputation_settings
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.thank_settings
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.thankwords
-UNION
-DISTINCT
-SELECT guild_id
-FROM repbot_schema.voice_activity
+SELECT guild_id FROM reputation_log
+UNION DISTINCT
+SELECT guild_id FROM abuse_protection
+UNION DISTINCT
+SELECT guild_id FROM active_categories
+UNION DISTINCT
+SELECT guild_id FROM active_channel
+UNION DISTINCT
+SELECT guild_id FROM announcements
+UNION DISTINCT
+SELECT guild_id FROM donor_roles
+UNION DISTINCT
+SELECT guild_id FROM guild_ranks
+UNION DISTINCT
+SELECT guild_id FROM guild_reactions
+UNION DISTINCT
+SELECT guild_id FROM guild_settings
+UNION DISTINCT
+SELECT guild_id FROM message_states
+UNION DISTINCT
+SELECT guild_id FROM receiver_roles
+UNION DISTINCT
+SELECT guild_id FROM donor_roles
+UNION DISTINCT
+SELECT guild_id FROM reputation_offset
+UNION DISTINCT
+SELECT guild_id FROM reputation_settings
+UNION DISTINCT
+SELECT guild_id FROM thank_settings
+UNION DISTINCT
+SELECT guild_id FROM thankwords
+UNION DISTINCT
+SELECT guild_id FROM voice_activity
 ORDER BY guild_id;
 
-UPDATE repbot_schema.cleanup_schedule
-SET user_id = 0
-WHERE user_id IS NULL;
+UPDATE repbot_schema.cleanup_schedule SET user_id = 0 WHERE user_id IS NULL;
 
 -- feature/analyzer-log
 CREATE TABLE repbot_schema.analyzer_results
 (
-    guild_id   BIGINT                                       NOT NULL,
-    channel_id BIGINT                                       NOT NULL,
-    message_id BIGINT                                       NOT NULL,
-    result     jsonb                                        NOT NULL,
-    analyzed   TIMESTAMP DEFAULT (NOW() AT TIME ZONE 'utc') NOT NULL,
+    guild_id   BIGINT NOT NULL,
+    channel_id BIGINT NOT NULL,
+    message_id BIGINT NOT NULL,
+    result     jsonb  NOT NULL,
+    analyzed   TIMESTAMP DEFAULT (now() AT TIME ZONE 'utc') NOT NULL,
     CONSTRAINT analyzer_results_pk
         PRIMARY KEY (guild_id, message_id)
 );
