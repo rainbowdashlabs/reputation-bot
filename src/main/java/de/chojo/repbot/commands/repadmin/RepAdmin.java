a6a49eb875fc59a4ee47ce191263c2f7c92880aa
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-11-17 18:28:25 +0100

    Enforce limits for number input (#441)

--- src/main/java/de/chojo/repbot/commands/repadmin/RepAdmin.java
+++ src/main/java/de/chojo/repbot/commands/repadmin/RepAdmin.java
@@ -9,8 +9,8 @@ import de.chojo.repbot.commands.repadmin.handler.Profile;
 import de.chojo.repbot.commands.repadmin.handler.reputation.Add;
 import de.chojo.repbot.commands.repadmin.handler.reputation.Remove;
 import de.chojo.repbot.commands.repadmin.handler.reputation.Set;
-import de.chojo.repbot.commands.repadmin.handler.resetdate.RemoveResetDate;
 import de.chojo.repbot.commands.repadmin.handler.resetdate.CurrentResetDate;
+import de.chojo.repbot.commands.repadmin.handler.resetdate.RemoveResetDate;
 import de.chojo.repbot.commands.repadmin.handler.resetdate.SetResetDate;
 import de.chojo.repbot.config.Configuration;
 import de.chojo.repbot.dao.provider.Guilds;
@@ -30,25 +30,40 @@ public class RepAdmin extends SlashCommand {
                                 .argument(Argument.user("user", "command.repadmin.reputation.add.user.description")
                                                   .asRequired())
                                 .argument(Argument.integer("amount", "command.repadmin.reputation.add.amount.description")
+                                                  .min(1)
+                                                  .max(1_000_000)
                                                   .asRequired()))
                         .subCommand(SubCommand.of("remove", "command.repadmin.reputation.remove.description")
                                 .handler(new Remove(roleAssigner, guilds))
                                 .argument(Argument.user("user", "command.repadmin.reputation.remove.user.description")
                                                   .asRequired())
                                 .argument(Argument.integer("amount", "command.repadmin.reputation.remove.amount.description")
+                                                  .min(1)
+                                                  .max(1_000_000)
                                                   .asRequired()))
                         .subCommand(SubCommand.of("set", "command.repadmin.reputation.set.description")
                                 .handler(new Set(roleAssigner, guilds))
                                 .argument(Argument.user("user", "command.repadmin.reputation.set.user.description")
                                                   .asRequired())
                                 .argument(Argument.integer("amount", "command.repadmin.reputation.set.amount.description")
+                                                  .min(0)
+                                                  .max(1_000_000)
                                                   .asRequired())))
                 .group(Group.of("resetdate", "command.repadmin.resetdate.description")
                         .subCommand(SubCommand.of("set", "command.repadmin.resetdate.set.description")
                                 .handler(new SetResetDate(guilds))
-                                .argument(Argument.integer("year", "command.repadmin.resetdate.set.year.description").asRequired().min(2016).max(LocalDate.now().getYear()))
-                                .argument(Argument.integer("month", "command.repadmin.resetdate.set.month.description").asRequired().min(1).max(12))
-                                .argument(Argument.integer("day", "command.repadmin.resetdate.set.day.description").asRequired().min(1).max(31)))
+                                .argument(Argument.integer("year", "command.repadmin.resetdate.set.year.description")
+                                                  .asRequired()
+                                                  .min(2016)
+                                                  .max(LocalDate.now().getYear() + 1))
+                                .argument(Argument.integer("month", "command.repadmin.resetdate.set.month.description")
+                                                  .asRequired()
+                                                  .min(1)
+                                                  .max(12))
+                                .argument(Argument.integer("day", "command.repadmin.resetdate.set.day.description")
+                                                  .asRequired()
+                                                  .min(1)
+                                                  .max(31)))
                         .subCommand(SubCommand.of("remove", "command.repadmin.resetdate.remove.description")
                                 .handler(new RemoveResetDate(guilds)))
                         .subCommand(SubCommand.of("current", "command.repadmin.resetdate.current.description")
