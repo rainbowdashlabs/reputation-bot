97e7725e920c8e4acd42114eb935211d9e46ecf3
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-12-23 15:42:16 +0100

    Migrate to new options locale code format (#460)

--- src/main/java/de/chojo/repbot/commands/log/Log.java
+++ src/main/java/de/chojo/repbot/commands/log/Log.java
@@ -18,16 +18,16 @@ public class Log extends SlashCommand {
                 .withPermission(Permission.MESSAGE_MANAGE)
                 .subCommand(SubCommand.of("received", "command.log.received.description")
                         .handler(new Received(guilds))
-                        .argument(Argument.user("user", "command.log.received.user.description").asRequired()))
+                        .argument(Argument.user("user", "command.log.received.options.user.description").asRequired()))
                 .subCommand(SubCommand.of("donated", "command.log.donated.description")
                         .handler(new Donated(guilds))
-                        .argument(Argument.user("user", "command.log.donated.user.description").asRequired()))
+                        .argument(Argument.user("user", "command.log.donated.options.user.description").asRequired()))
                 .subCommand(SubCommand.of("message", "command.log.message.description")
                         .handler(new Message(guilds))
-                        .argument(Argument.text("messageid", "command.log.message.messageid.description").asRequired()))
+                        .argument(Argument.text("messageid", "command.log.message.options.messageid.description").asRequired()))
                 .subCommand(SubCommand.of("analyzer", "command.log.analyzer.description")
                         .handler(new Analyzer(guilds))
-                        .argument(Argument.text("messageid", "command.log.analyzer.messageid.description").asRequired()))
+                        .argument(Argument.text("messageid", "command.log.analyzer.options.messageid.description").asRequired()))
         );
     }
 }
