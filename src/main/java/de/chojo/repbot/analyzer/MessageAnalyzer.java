91cfd344ce5ef8044eeb37cbaeeff7aa4e739e11
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-04-30 20:11:22 +0200

    Handle sub timeout exception

--- src/main/java/de/chojo/repbot/analyzer/MessageAnalyzer.java
+++ src/main/java/de/chojo/repbot/analyzer/MessageAnalyzer.java
@@ -13,7 +13,6 @@ import de.chojo.repbot.dao.access.guild.settings.Settings;
 import de.chojo.repbot.dao.provider.Guilds;
 import de.chojo.repbot.dao.provider.Metrics;
 import de.chojo.repbot.util.LogNotify;
-
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageType;
@@ -21,13 +20,10 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 
-import javax.validation.constraints.Null;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.regex.Pattern;
@@ -41,9 +37,9 @@ public class MessageAnalyzer {
     private static final Object PLACEHOLDER = new Object();
     private final ContextResolver contextResolver;
     private final Cache<Long, AnalyzerResult> resultCache = CacheBuilder.newBuilder()
-                                                                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                                                                        .maximumSize(100000)
-                                                                        .build();
+            .expireAfterWrite(10, TimeUnit.MINUTES)
+            .maximumSize(100000)
+            .build();
     private final Cache<Long, Object> rejectionCache = CacheBuilder.newBuilder()
             .expireAfterWrite(1, TimeUnit.MINUTES)
             .build();
@@ -80,6 +76,9 @@ public class MessageAnalyzer {
             if (e.getCause() instanceof TimeoutException) {
                 log.warn(LogNotify.NOTIFY_ADMIN, "Timeout when analyzing message using pattern {} for guild {}", pattern.pattern(), settings.guildId());
                 rejectionCache.put(settings.guildId(), PLACEHOLDER);
+            } else if (e.getCause().getCause() instanceof TimeoutException) {
+                log.warn(LogNotify.NOTIFY_ADMIN, "Timeout when analyzing message using pattern {} for guild {}", pattern.pattern(), settings.guildId());
+                rejectionCache.put(settings.guildId(), PLACEHOLDER);
             } else {
                 log.error("Could not compute analyzer result", e);
             }
@@ -106,7 +105,8 @@ public class MessageAnalyzer {
         if (message.getType() == MessageType.INLINE_REPLY) {
 
             var referencedMessage = message.getReferencedMessage();
-            if (referencedMessage == null) return AnalyzerResult.empty(match, EmptyResultReason.REFERENCE_MESSAGE_NOT_FOUND);
+            if (referencedMessage == null)
+                return AnalyzerResult.empty(match, EmptyResultReason.REFERENCE_MESSAGE_NOT_FOUND);
 
             Member user;
 
@@ -201,15 +201,15 @@ public class MessageAnalyzer {
         }
 
         memberMatches = memberMatches.stream()
-                                     .filter(e -> e.score() >= configuration.analyzerSettings().minFuzzyScore())
-                                     .toList();
+                .filter(e -> e.score() >= configuration.analyzerSettings().minFuzzyScore())
+                .toList();
 
         var members = users.stream()
-                           .filter(e -> e.getWeight() >= configuration.analyzerSettings().minFuzzyScore())
-                           .distinct()
-                           .sorted()
-                           .limit(limit)
-                           .collect(Collectors.toList());
+                .filter(e -> e.getWeight() >= configuration.analyzerSettings().minFuzzyScore())
+                .distinct()
+                .sorted()
+                .limit(limit)
+                .collect(Collectors.toList());
         if (members.isEmpty()) return AnalyzerResult.empty(matchPattern, EmptyResultReason.INSUFFICIENT_SCORE);
 
         var thankwords = thankWordIndices.stream().map(words::get).collect(Collectors.toList());
