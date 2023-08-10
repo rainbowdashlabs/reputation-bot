2f1b14420b5a8c31762a57cdf7ff4ee336829bc8
Lilly Tempest <46890129+rainbowdashlabs@users.noreply.github.com>
2023-08-10 18:30:43 +0200

    Refactor Reputation Analyzer and add tests (#542)

--- src/main/java/de/chojo/repbot/service/reputation/SubmitResult.java
+++ src/main/java/de/chojo/repbot/service/reputation/SubmitResult.java
@@ -5,6 +5,8 @@
  */
 package de.chojo.repbot.service.reputation;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import de.chojo.jdautil.localization.util.Replacement;
 
 import java.util.List;
@@ -12,6 +14,11 @@ import java.util.List;
 public record SubmitResult(SubmitResultType type, List<Replacement> replacements) {
 
     public static SubmitResult of(SubmitResultType type, Replacement... replacements){
-        return new SubmitResult(type, List.of(replacements));
+        return of(type, List.of(replacements));
+    }
+
+    @JsonCreator
+    public static SubmitResult of(@JsonProperty("type") SubmitResultType type,  @JsonProperty("replacements") List<Replacement> replacements){
+        return new SubmitResult(type, replacements);
     }
 }
