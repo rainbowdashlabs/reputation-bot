60735784fb205c72d90d759b1f3722e9b2fcfcc1
Lilly Tempest <46890129+rainbowdashlabs@users.noreply.github.com>
2023-11-03 17:34:35 +0100

    Feature/udp logging (#557)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
+
 plugins {
     alias(libs.plugins.shadow)
     alias(libs.plugins.spotless)
@@ -81,6 +83,7 @@ tasks {
     }
 
     shadowJar {
+        transform(Log4j2PluginsCacheFileTransformer::class.java)
         mergeServiceFiles()
         manifest {
             attributes(mapOf("Main-Class" to "de.chojo.repbot.ReputationBot"))
