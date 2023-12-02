2cf913ea7609c2b10ae2bee502266953916e626a
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-12-02 19:05:39 +0100

    Cleanup

--- src/main/java/de/chojo/repbot/commands/log/handler/Analyzer.java
+++ src/main/java/de/chojo/repbot/commands/log/handler/Analyzer.java
@@ -16,7 +16,7 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 
 import java.util.List;
 
-public class Analyzer implements SlashHandler {
+public class Analyzer extends BaseAnalyzer implements SlashHandler  {
     private final Guilds guilds;
 
     public Analyzer(Guilds guilds) {
@@ -25,33 +25,6 @@ public class Analyzer implements SlashHandler {
 
     @Override
     public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context) {
-        var optMessageId = ValueParser.parseLong(event.getOption("messageid").getAsString());
-        if (optMessageId.isEmpty()) {
-            event.reply(context.localize("error.invalidMessage")).setEphemeral(true).queue();
-            return;
-        }
-        var reputation = guilds.guild(event.getGuild()).reputation();
-        var resultEntry = reputation.analyzer()
-                                    .get(optMessageId.get());
-
-        if (resultEntry.isEmpty()) {
-            event.reply(context.localize("command.log.analyzer.notanalyzed")).setEphemeral(true).queue();
-            return;
-        }
-
-        var embed = resultEntry.get().embed(event.getGuild(), context);
-
-        var reputationLogEntries = reputation.log().messageLog(optMessageId.get(), 10);
-
-        var entries = LogFormatter.mapMessageLogEntry(context, reputationLogEntries);
-
-        var builder = new LocalizedEmbedBuilder(context.guildLocalizer())
-                .setTitle("command.log.message.message.log", Replacement.create("ID", optMessageId.get()));
-
-        LogFormatter.buildFields(entries, builder);
-
-        embed.add(builder.build());
-
-        event.replyEmbeds(embed).setEphemeral(true).queue();
+        onSlashCommand(event, context, guilds.guild(event.getGuild()).reputation());
     }
 }
---
+++ src/main/java/de/chojo/repbot/commands/log/handler/BaseAnalyzer.java
@@ -0,0 +1,42 @@
+package de.chojo.repbot.commands.log.handler;
+
+import de.chojo.jdautil.localization.util.LocalizedEmbedBuilder;
+import de.chojo.jdautil.localization.util.Replacement;
+import de.chojo.jdautil.parsing.ValueParser;
+import de.chojo.jdautil.wrapper.EventContext;
+import de.chojo.repbot.dao.access.guild.reputation.Reputation;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class BaseAnalyzer {
+    public void onSlashCommand(SlashCommandInteractionEvent event, EventContext context, Reputation reputation) {
+        var optMessageId = ValueParser.parseLong(event.getOption("message_id").getAsString());
+        if (optMessageId.isEmpty()) {
+            event.reply(context.localize("error.invalidMessage")).setEphemeral(true).queue();
+            return;
+        }
+
+        var resultEntry = reputation.analyzer()
+                                    .get(optMessageId.get());
+
+        if (resultEntry.isEmpty()) {
+            event.reply(context.localize("command.log.analyzer.notanalyzed")).setEphemeral(true).queue();
+            return;
+        }
+
+        var embed = resultEntry.get().embed(event.getGuild(), context);
+
+        var reputationLogEntries = reputation.log().messageLog(optMessageId.get(), 10);
+
+        var entries = LogFormatter.mapMessageLogEntry(context, reputationLogEntries);
+
+        var builder = new LocalizedEmbedBuilder(context.guildLocalizer())
+                .setTitle("command.log.message.message.log", Replacement.create("ID", optMessageId.get()));
+
+        LogFormatter.buildFields(entries, builder);
+
+        embed.add(builder.build());
+
+        event.replyEmbeds(embed).setEphemeral(true).queue();
+
+    }
+}
