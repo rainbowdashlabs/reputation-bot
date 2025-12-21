42b3b0e1682ff999223090f219cae3b0f230174e
Nora <46890129+RainbowDashLabs@users.noreply.github.com>
2025-12-21 02:10:43 +0100

    Enhance api logging

--- src/main/java/de/chojo/repbot/web/Api.java
+++ src/main/java/de/chojo/repbot/web/Api.java
@@ -5,15 +5,16 @@
  */
 package de.chojo.repbot.web;
 
+import com.google.common.collect.Streams;
 import de.chojo.repbot.dao.provider.Metrics;
-import de.chojo.repbot.web.error.ApiException;
 import de.chojo.repbot.web.routes.v1.MetricsRoute;
-import io.javalin.Javalin;
-import io.javalin.apibuilder.ApiBuilder;
-import io.javalin.apibuilder.EndpointGroup;
-import io.javalin.router.Endpoint;
+import io.javalin.http.ContentType;
 import org.slf4j.Logger;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static io.javalin.apibuilder.ApiBuilder.after;
 import static io.javalin.apibuilder.ApiBuilder.before;
 import static io.javalin.apibuilder.ApiBuilder.path;
 import static org.slf4j.LoggerFactory.getLogger;
@@ -27,7 +28,24 @@ public class Api {
     }
 
     public void init() {
-        before(ctx -> log.debug("Received request on {}.", ctx.path()));
+        before(ctx -> log.trace("Received request on route: {} {}\nHeaders:\n{}\nBody:\n{}",
+                ctx.method() + " " + ctx.url(),
+                ctx.queryString(),
+                ctx.headerMap().entrySet().stream().map(h -> "   " + h.getKey() + ": " + h.getValue())
+                   .collect(Collectors.joining("\n")),
+                ctx.body().substring(0, Math.min(ctx.body().length(), 180))));
+        after(ctx -> {
+            log.trace("Answered request on route: {} {}\nStatus: {}\nHeaders:\n{}\nBody:\n{}",
+                    ctx.method() + " " + ctx.url(),
+                    ctx.queryString(),
+                    ctx.status(),
+                    Streams.stream(ctx.req().getHeaderNames().asIterator()).map(h -> "   " + h + ": " + ctx.res().getHeader(h))
+                           .collect(Collectors.joining("\n")),
+                    ContentType.OCTET_STREAM.equals(ctx.contentType()) ? "Bytes"
+                            : Objects.requireNonNullElse(ctx.result(), "")
+                                     .substring(0, Math.min(
+                                             Objects.requireNonNullElse(ctx.result(), "").length(), 180)));
+        });
         path("v1", () -> path("metrics", metricsRoute::buildRoutes));
     }
 }
