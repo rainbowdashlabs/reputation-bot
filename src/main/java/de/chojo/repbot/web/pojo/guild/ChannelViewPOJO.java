c279bbdffd7b992d16023f64537fdc5695867fdb
Nora <46890129+RainbowDashLabs@users.noreply.github.com>
2026-02-02 17:44:05 +0100

    Apply spotless

--- src/main/java/de/chojo/repbot/web/pojo/guild/ChannelViewPOJO.java
+++ src/main/java/de/chojo/repbot/web/pojo/guild/ChannelViewPOJO.java
@@ -1,3 +1,8 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) RainbowDashLabs and Contributor
+ */
 package de.chojo.repbot.web.pojo.guild;
 
 import de.chojo.repbot.web.pojo.guild.channel.CategoryPOJO;
